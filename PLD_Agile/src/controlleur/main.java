82ce68f2dd2f62119a6090e28d3ed2f5f11605b2
ThibNoilly <thibault.noilly@gmail.com>
2016-10-14 10:38:48 +0200

    Ceci est un test (Thibault)

--- PLD_Agile/src/controlleur/main.java
+++ PLD_Agile/src/controlleur/main.java
@@ -5,5 +5,16 @@ import vue.Fenetre;
 public class main {
 	public static void main(String[] args){
 		Fenetre fenetre = new Fenetre("optimod",400,400);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		System.out.println("Ceci n'est pas une phrase.");
 	}
 }
