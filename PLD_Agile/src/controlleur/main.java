553200f1cb5380223301b0ff5776667bdda479a1
ThibNoilly <thibault.noilly@gmail.com>
2016-10-14 10:49:22 +0200

    TEST Push n°12548155422 (j'ai soif)

--- PLD_Agile/src/controlleur/main.java
+++ PLD_Agile/src/controlleur/main.java
@@ -5,18 +5,9 @@ import vue.Fenetre;
 public class main {
 	public static void main(String[] args){
 		Fenetre fenetre = new Fenetre("optimod",400,400);
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		System.out.println("Ceci n'est pas une phrase.");
 		System.out.println("yolo");
 		System.out.println("je suis youssef chettar!");
+		
+		System.out.println("Bonjour, je m'appelle Thibault, je sais compter 2 par 2 mais ce putain de truc ne veut pas push");
 	}
 }
