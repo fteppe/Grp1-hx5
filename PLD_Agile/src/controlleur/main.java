1a8f45739c4f322a97b6e058a20ab2bd449511f4
Nathan <Nathan@10.43.5.12>
2016-10-14 10:51:39 +0200

    commit de test

--- PLD_Agile/src/controlleur/main.java
+++ PLD_Agile/src/controlleur/main.java
@@ -4,6 +4,8 @@ import vue.Fenetre;
 
 public class main {
 	public static void main(String[] args){
+		System.out.println("scrum manager");
 		Fenetre fenetre = new Fenetre("optimod",400,400);
+		
 	}
 }
