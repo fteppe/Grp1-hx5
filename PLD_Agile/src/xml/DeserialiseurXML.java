c7390d857f8538beded8b9ce69e1d7469df1de04
afossart <alexis.fossart@insa-lyon.fr>
2016-10-14 10:45:39 +0200

    deserialiseur prêt a être rempli

--- PLD_Agile/src/xml/DeserialiseurXML.java
+++ PLD_Agile/src/xml/DeserialiseurXML.java
@@ -24,19 +24,44 @@ public class DeserialiseurXML {
 	 * @throws IOException
 	 * @throws ExceptionXML
 	 */
-	public static void charger(Plan plan) throws ParserConfigurationException, SAXException, IOException, ExceptionXML{
+	public static void chargerPlan(Plan plan) throws ParserConfigurationException, SAXException, IOException, ExceptionXML{
 		File xml = OuvreurDeFichierXML.getInstance().ouvre(true);
         DocumentBuilder docBuilder = DocumentBuilderFactory.newInstance().newDocumentBuilder();	
         Document document = docBuilder.parse(xml);
         Element racine = document.getDocumentElement();
-        if (racine.getNodeName().equals("plan")) {
-           construireAPartirDeDOMXML(racine, plan);
+        if (racine.getNodeName().equals("reseau")) {
+           construirePlanAPartirDeDOMXML(racine, plan);
         }
         else
         	throw new ExceptionXML("Document non conforme");
 	}
 
-    private static void construireAPartirDeDOMXML(Element noeudDOMRacine, Plan plan) throws ExceptionXML, NumberFormatException{
+	/**
+	 * Ouvre un fichier xml et charge les livraisons a partir du contenu du fichier
+	 * @param plan
+	 * @throws ParserConfigurationException
+	 * @throws SAXException
+	 * @throws IOException
+	 * @throws ExceptionXML
+	 */
+	public static void chargerLivraisons(Plan plan) throws ParserConfigurationException, SAXException, IOException, ExceptionXML{
+		File xml = OuvreurDeFichierXML.getInstance().ouvre(true);
+        DocumentBuilder docBuilder = DocumentBuilderFactory.newInstance().newDocumentBuilder();	
+        Document document = docBuilder.parse(xml);
+        Element racine = document.getDocumentElement();
+        if (racine.getNodeName().equals("demandeDeLivraisons")) {
+           construireLivraisonsAPartirDeDOMXML(racine, plan);
+        }
+        else
+        	throw new ExceptionXML("Document non conforme");
+	}
+	
+    private static void construirePlanAPartirDeDOMXML(Element noeudDOMRacine, Plan plan) throws ExceptionXML, NumberFormatException{
+    	
+    }
+    
+
+    private static void construireLivraisonsAPartirDeDOMXML(Element noeudDOMRacine, Plan plan) throws ExceptionXML, NumberFormatException{
     	
     }
     
