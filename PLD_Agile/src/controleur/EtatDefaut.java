d33e4f2d11068451349adbafbb8cd2828962cb60
nathan arsac <nathan.arsac@insa-lyon.fr>
2016-10-18 09:39:03 +0200

    Avancement controleur : Ajout des changements d'états, implémentation des méthodes

--- PLD_Agile/src/controleur/EtatDefaut.java
+++ PLD_Agile/src/controleur/EtatDefaut.java
@@ -1,12 +1,30 @@
 package controleur;
 
 import vue.Fenetre;
+import xml.DeserialiseurXML;
+import xml.ExceptionXML;
+
+import java.io.IOException;
+
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.xml.sax.SAXException;
+
 import modele.Plan;
 
 public abstract class EtatDefaut implements Etat {
 	// Definition des comportements par defaut des methodes
 	
-	public void chargerPlan(Controleur controleur, Plan plan, Fenetre fenetre){}
+	public void chargerPlan(Controleur controleur, Plan plan, Fenetre fenetre) {
+		try {
+			DeserialiseurXML.chargerPlan(plan);
+		} catch (ParserConfigurationException 
+				| SAXException | IOException 
+				| ExceptionXML | NumberFormatException e) {
+		}
+		// fenetre.afficherMessage(e);
+		controleur.setEtatCourant(controleur.EtatPlanCharge);
+	}
 	
 	public void chargerDemandeLivraison(Controleur controleur, Plan plan, Fenetre fenetre){}
 
--- PLD_Agile/src/controleur/EtatTourneeCalculee.java
+++ PLD_Agile/src/controleur/EtatTourneeCalculee.java
@@ -1,24 +1,34 @@
 package controleur;
 
+import java.io.IOException;
+
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.xml.sax.SAXException;
+
 import modele.Plan;
 import vue.Fenetre;
+import xml.DeserialiseurXML;
+import xml.ExceptionXML;
 
 public class EtatTourneeCalculee extends EtatDefaut {
 	// Etat atteint une fois que la tournée est calculée
-
-	@Override
-	public void chargerPlan(Controleur controleur, Plan plan, Fenetre fenetre) {
-		
-	}
 	
 	@Override
 	public void chargerDemandeLivraison(Controleur controleur, Plan plan, Fenetre fenetre) {
-		
+	    try {
+		DeserialiseurXML.chargerLivraisons(plan);
+	    } catch (ParserConfigurationException 
+			| SAXException | IOException 
+			| ExceptionXML | NumberFormatException e) {
+	    }
+	    // fenetre.afficherMessage(e);
+	    controleur.setEtatCourant(controleur.EtatDemandeLivraisonCharge);
 	}
 	
 	@Override
 	public void quitter() {
-		
+	    System.exit(0);
 	}
 	
 	/*
