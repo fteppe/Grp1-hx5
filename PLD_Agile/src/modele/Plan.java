c55771b230dee54882043e6de8067d34eb4662ac
Brunet Devon <devon.brunet@laposte.net>
2016-10-18 09:22:51 +0200

    Placement Sommet en private

--- PLD_Agile/src/modele/Plan.java
+++ PLD_Agile/src/modele/Plan.java
@@ -224,4 +224,57 @@ public class Plan extends Observable {
        return this.listeTroncons.get(id);
    }
 
+   private enum Etat {
+       GRIS,
+       NOIR,
+       BLANC
+   }
+   
+   private class Sommet implements Comparable<Sommet> {
+
+       private int id;
+       private int position;
+       private double cout;
+       private Etat etat;
+       
+       public Sommet(int id, int position, double cout, Etat etat){
+   	this.id = id;
+   	this.position = position;
+   	this.cout = cout;
+   	this.etat = etat;
+       }
+       
+       @Override
+       public int compareTo(Sommet autre) {
+   	// TODO Auto-generated method stub
+   	return Double.compare(this.cout, autre.cout);
+       }
+
+       public int getId() {
+           return id;
+       }
+
+       public int getPosition() {
+           return position;
+       }
+
+       public double getCout() {
+           return cout;
+       }
+       
+       public Etat getEtat() {
+           return etat;
+       }
+       
+       public void setEtat(Etat nouvelEtat) {
+   	this.etat = nouvelEtat;
+       }
+       
+       public void setCout(double nouveauCout) {
+   	this.cout = nouveauCout;
+       }
+       
+
+   }
+
 }
