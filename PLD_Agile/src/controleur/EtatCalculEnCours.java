061745645947fd8fd2c68cd9609160f7bb7f64c1
nathan arsac <nathan.arsac@insa-lyon.fr>
2016-11-04 08:45:23 +0100

    Ajout classe EtatCalculEnCours

---
+++ PLD_Agile/src/controleur/EtatCalculEnCours.java
@@ -0,0 +1,39 @@
+package controleur;
+
+import java.io.IOException;
+
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.xml.sax.SAXException;
+
+import modele.Plan;
+import vue.Fenetre;
+import xml.DeserialiseurXML;
+import xml.ExceptionXML;
+
+public class EtatCalculEnCours extends EtatDefaut {
+	// Etat lorsque le calcul de la tournee est en cours
+	
+	
+@Override
+public void chargerDemandeLivraison(Controleur controleur, Plan plan, Fenetre fenetre) {
+    try {
+    	DeserialiseurXML.chargerLivraisons(plan);
+    	plan.setTournee(null);
+    	fenetre.afficherMessage("Demande de livraison charg�e");
+    	fenetre.afficherDetailDemandeLivraison();
+    	controleur.setEtatCourant(controleur.ETAT_DEMANDE_LIVRAISON_CHARGE);
+    } catch (ParserConfigurationException 
+    		| SAXException | IOException 
+    		| ExceptionXML | NumberFormatException e) {
+    	fenetre.afficherMessage(e.getMessage());
+    }
+    }
+    
+    @Override
+    public void quitter() {
+    System.exit(0);
+    }
+    
+
+}
