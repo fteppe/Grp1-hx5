1e0f9dddbc5153b20ecb69ab05b71e904b4d5e8f
nathan arsac <nathan.arsac@insa-lyon.fr>
2016-11-05 21:07:44 +0100

    esthetique de code

--- PLD_Agile/src/controleur/EtatDemandeLivraisonCharge.java
+++ PLD_Agile/src/controleur/EtatDemandeLivraisonCharge.java
@@ -42,13 +42,11 @@ public class EtatDemandeLivraisonCharge extends EtatDefaut {
 	    // Operations effectuees dans l'etat calcul en cours
 	    if(plan.calculerTournee(tempsLimite)){
 			fenetre.afficherMessage("Tournée calculée");
-			controleur.setEtatCourant(controleur.ETAT_TOURNEE_CALCULEE);
 	    }
 	    else {
 		fenetre.afficherMessage("Temps limite atteint, si aucune tournée n'a été trouvée, veuillez selectionner un temps limite plus long");
-		controleur.setEtatCourant(controleur.ETAT_TOURNEE_CALCULEE);
 	    }
-		
+	    controleur.setEtatCourant(controleur.ETAT_TOURNEE_CALCULEE);
 	    	
 	}
 }
