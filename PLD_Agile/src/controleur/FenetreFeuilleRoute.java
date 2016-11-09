8ecdb5d5c7731f20c61fcaf8a38d39c5f39c00a8
tetramir <teppe.florent@hotmail.fr>
2016-11-10 00:08:27 +0100

    minor bug fixes

---
+++ PLD_Agile/src/controleur/FenetreFeuilleRoute.java
@@ -0,0 +1,31 @@
+package controleur;
+
+import java.awt.ScrollPane;
+
+import javax.swing.BorderFactory;
+import javax.swing.JDialog;
+import javax.swing.JPopupMenu;
+import javax.swing.JScrollBar;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.border.BevelBorder;
+import javax.swing.border.Border;
+
+import modele.Plan;
+
+public class FenetreFeuilleRoute extends JDialog{
+	private JScrollPane scroll;
+	private JTextArea feuilleRoute;
+	public FenetreFeuilleRoute(Plan plan){
+		super();
+		feuilleRoute = new JTextArea(40,30);
+		feuilleRoute.setEditable(false);
+		setResizable(false);
+		feuilleRoute.setBorder(BorderFactory.createBevelBorder(BevelBorder.LOWERED));
+		scroll = new JScrollPane(feuilleRoute);
+		add(scroll);
+		pack();
+		setTitle("feuille de route");
+		setVisible(true);
+	}
+}
--- PLD_Agile/src/vue/Fenetre.java
+++ PLD_Agile/src/vue/Fenetre.java
@@ -10,6 +10,7 @@ import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 
 import controleur.Controleur;
+import controleur.FenetreFeuilleRoute;
 import modele.Plan;
 
 public class Fenetre extends JFrame{
@@ -33,7 +34,7 @@ public class Fenetre extends JFrame{
 	private PopMenuLivraisonAjout popupMenuLivraisonAjout;
 	private PopMenuIntersection popupMenuIntersection;
 	private MenuCreationLivraison menuCreationLivraison;
-	
+	private FenetreFeuilleRoute feuilleRoute;
 	
 	protected Controleur controleur;
 	
@@ -57,8 +58,9 @@ public class Fenetre extends JFrame{
 	    this.setTitle(titreFenetre);
 	    this.setSize((int)dimensions.x,(int)dimensions.y);
 	    this.setLocationRelativeTo(null);
-
-	    //menuCreationLivraison = new MenuCreationLivraison(this, 25, true, new Point(0,0));
+	    
+	    //feuilleRoute = new FenetreFeuilleRoute(plan);
+	    
 	    console = new JTextArea(3, 10);
 	    console.setEditable(false);
 	    console.setBorder(BorderFactory.createLoweredBevelBorder());
