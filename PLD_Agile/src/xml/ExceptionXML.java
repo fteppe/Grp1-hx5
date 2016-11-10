21290c720a17750e99ca37567e1911f79370ebb5
Brunet Devon <devon.brunet@laposte.net>
2016-11-10 21:23:35 +0100

    Ajout commentaires

--- PLD_Agile/src/xml/ExceptionXML.java
+++ PLD_Agile/src/xml/ExceptionXML.java
@@ -1,7 +1,11 @@
 package xml;
 
+/**
+ * Cette classe permet de gerer les exceptions liees a lecture
+ * de fichiers XML.
+ *
+ */
 public class ExceptionXML extends Exception {
-
 	public ExceptionXML(String message) {
 		super(message);
 	}
