8377ebfa7397fe31de309d78ec74c051153d733c
tetramir <teppe.florent@hotmail.fr>
2017-01-06 10:55:06 +0100

    IHM changes

--- IHM/RestIf/app/src/main/java/com/ihm/h4105/restif/MainActivity.java
+++ IHM/RestIf/app/src/main/java/com/ihm/h4105/restif/MainActivity.java
@@ -92,20 +92,18 @@ public class MainActivity extends AppCompatActivity
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
+
+        View target = findViewById(R.id.notif);
+
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.notif) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.restaurant) {
 
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.with) {
 
         }
 
